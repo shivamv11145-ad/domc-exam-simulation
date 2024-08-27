b11f1d47d3ebdabac180bac6b75f70bd2dc66412
shivamv11145-ad <shivam.verma@cyderes.com>
2024-08-27 20:52:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let isCurrentQuestionCorrect = true;
 // Fetch questions from the JSON file
 async function loadQuestions() {
     try {
-        const response = await fetch('questions3.json');
+        const response = await fetch('questions5.json');
         questions = await response.json();
         startExam();
     } catch (error) {
